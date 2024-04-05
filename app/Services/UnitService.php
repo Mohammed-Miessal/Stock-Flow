4901a28ff06dd1f9bec14f5f20bbd3e0a62b7035
Mohammed <mohammedmiessal@gmail.com>
2024-04-05 10:49:22 +0000

    Update the UnitService.php  file

--- app/Services/UnitService.php
+++ app/Services/UnitService.php
@@ -16,35 +16,30 @@ class UnitService implements UnitInterface
 
     public function index()
     {
-        // return Unit::all();
         $units = $this->unit->index();
         return $units;
     }
 
     public function show($id)
     {
-        // return Unit::find($id);
         $unit = $this->unit->show($id);
         return $unit;
     }
 
     public function create($data)
     {
-        // return Unit::create($data);
         $unit = $this->unit->create($data);
         return $unit;
     }
 
     public function update($id, $data)
     {
-        // return Unit::find($id)->update($data);
         $unit = $this->unit->update($id, $data);
         return $unit;
     }
 
     public function delete($id)
     {
-        // return Unit::find($id)->delete();
         $unit = $this->unit->delete($id);
         return $unit;
     }
