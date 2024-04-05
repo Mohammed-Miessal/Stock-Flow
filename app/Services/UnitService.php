8e4ebda99e00891bdb2e56dae55b70e58d47c541
Mohammed <mohammedmiessal@gmail.com>
2024-04-05 10:32:41 +0000

    Unit Service Updated to the correct form

--- app/Services/UnitService.php
+++ app/Services/UnitService.php
@@ -9,33 +9,43 @@ class UnitService implements UnitInterface
 
     protected $unit;
 
-    public function __construct(Unit $unit)
+    public function __construct(UnitInterface $unit)
     {
         $this->unit = $unit;
     }
 
     public function index()
     {
-        return Unit::all();
+        // return Unit::all();
+        $units = $this->unit->index();
+        return $units;
     }
 
     public function show($id)
     {
-        return Unit::find($id);
+        // return Unit::find($id);
+        $unit = $this->unit->show($id);
+        return $unit;
     }
 
     public function create($data)
     {
-        return Unit::create($data);
+        // return Unit::create($data);
+        $unit = $this->unit->create($data);
+        return $unit;
     }
 
     public function update($id, $data)
     {
-        return Unit::find($id)->update($data);
+        // return Unit::find($id)->update($data);
+        $unit = $this->unit->update($id, $data);
+        return $unit;
     }
 
     public function delete($id)
     {
-        return Unit::find($id)->delete();
+        // return Unit::find($id)->delete();
+        $unit = $this->unit->delete($id);
+        return $unit;
     }
 }
