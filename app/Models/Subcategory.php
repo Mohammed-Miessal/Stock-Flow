ae8b0d367c304857d7e1b521ddb911495197f0b2
Mohammed <mohammedmiessal@gmail.com>
2024-04-06 18:41:23 +0000

    Fix Subcategory Model and Seeder

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -15,4 +15,7 @@ class Subcategory extends Model
     {
         return $this->belongsTo(Category::class);
     }
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
--- database/seeders/SubcategorySeeder.php
+++ database/seeders/SubcategorySeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Subcategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class SubcategorySeeder extends Seeder
 {
@@ -12,6 +13,15 @@ class SubcategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for ($i = 1; $i <= 10; $i++) {
+          Subcategory::create([
+                'name' => "Subcategory $i",
+                'subcategory_code' => "SUB$i",
+                'description' => "Description of Subcategory $i",
+                'category_id' => $i,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
