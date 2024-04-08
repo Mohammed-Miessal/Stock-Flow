a45391ba6928a7d25502a4d4a3f969a927976a34
Mohammed <mohammedmiessal@gmail.com>
2024-04-08 00:07:53 +0000

    Update the read view for the Product Model

--- resources/views/chapters/Product/read.blade.php
+++ resources/views/chapters/Product/read.blade.php
@@ -36,12 +36,12 @@
                                         <th class="px-4 py-3">quantity</th>
                                         <th class="px-4 py-3">unit</th>
                                         <th class="px-4 py-3">price</th>
-                                        <th class="px-4 py-3">status</th>
+                                        {{-- <th class="px-4 py-3">status</th>
                                         <th class="px-4 py-3">category</th>
                                         <th class="px-4 py-3">tax</th>
                                         <th class="px-4 py-3">supplier</th>
-                                        <th class="px-4 py-3">subcategorie</th>
-                                        <th class="px-4 py-3">actions</th>
+                                        <th class="px-4 py-3">subcategorie</th> --}}
+                                        <th class="px-4 py-3  ">actions</th>
                                     </tr>
                                 </thead>
                                 <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
@@ -68,7 +68,7 @@
                                             <td class="px-4 py-3 text-sm">
                                                 {{ $product->price }}
                                             </td>
-                                            <td class="px-4 py-3 text-sm">
+                                            {{-- <td class="px-4 py-3 text-sm">
                                                 {{ $product->status }}
                                             </td>
                                             <td class="px-4 py-3 text-sm">
@@ -82,16 +82,16 @@
                                             </td>
                                             <td class="px-4 py-3 text-sm">
                                                 {{ $product->subcategory->name }}
-                                            </td>
+                                            </td> --}}
                                             <!-- Action -->
                                             <td class="px-4 py-3">
                                                 <div class="flex items-center space-x-4 text-sm">
                                                     {{-- Show --}}
                                                     <a href="{{ route('product.show', $product->id) }}">
                                                         <button
-                                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                            aria-label="Edit">
-                                                            <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg"
+                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                        aria-label="Show">
+                                                            <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg"
                                                                 viewBox="0 0 24 24" fill="currentColor">
                                                                 <path
                                                                     d="M12.0003 3C17.3924 3 21.8784 6.87976 22.8189 12C21.8784 17.1202 17.3924 21 12.0003 21C6.60812 21 2.12215 17.1202 1.18164 12C2.12215 6.87976 6.60812 3 12.0003 3ZM12.0003 19C16.2359 19 19.8603 16.052 20.7777 12C19.8603 7.94803 16.2359 5 12.0003 5C7.7646 5 4.14022 7.94803 3.22278 12C4.14022 16.052 7.7646 19 12.0003 19ZM12.0003 16.5C9.51498 16.5 7.50026 14.4853 7.50026 12C7.50026 9.51472 9.51498 7.5 12.0003 7.5C14.4855 7.5 16.5003 9.51472 16.5003 12C16.5003 14.4853 14.4855 16.5 12.0003 16.5ZM12.0003 14.5C13.381 14.5 14.5003 13.3807 14.5003 12C14.5003 10.6193 13.381 9.5 12.0003 9.5C10.6196 9.5 9.50026 10.6193 9.50026 12C9.50026 13.3807 10.6196 14.5 12.0003 14.5Z">
@@ -118,10 +118,10 @@
                                                         @csrf
                                                         @method('DELETE')
 
-                                                        <button type="submit"
-                                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                            aria-label="Delete">
-                                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                        <button
+                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                        aria-label="Delete">
+                                                            <svg class="w-4 h-4" aria-hidden="true" fill="currentColor"
                                                                 viewBox="0 0 20 20">
                                                                 <path fill-rule="evenodd"
                                                                     d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
@@ -130,7 +130,7 @@
                                                         </button>
 
                                                     </form>
-                                                </div>
+                                                {{-- </div> --}}
                                             </td>
                                         </tr>
                                     @endforeach
