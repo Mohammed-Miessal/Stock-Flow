41582706cddbb118ca359a7b3b9ad249386abb8c
Mohammed <mohammedmiessal@gmail.com>
2024-04-13 23:58:54 +0000

    Update the profile pictures

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -99,7 +99,7 @@
                        @click="toggleProfileMenu" @keydown.escape="closeProfileMenu" aria-label="Account"
                        aria-haspopup="true">
                        <img class="object-cover w-8 h-8 rounded-full"
-                           src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
+                           src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
                            alt="" aria-hidden="true" />
                    </button>
                    <template x-if="isProfileMenuOpen">
@@ -153,4 +153,3 @@
            </ul>
        </div>
    </header>
-   
