1b328616f0964522dcc3323a3f51488c3cbb05bc
Mohammed <mohammedmiessal@gmail.com>
2024-04-17 10:05:19 +0100

    Invoice views are fixed

--- resources/views/chapters/Invoice/edit.blade.php
+++ resources/views/chapters/Invoice/edit.blade.php
@@ -54,7 +54,8 @@
                                                 Date
                                             </span>
 
-                                            <input type="date" required name="invoice_date" value="{{ $invoice->invoice_date }}"
+                                            <input type="date" required name="invoice_date"
+                                                value="{{ $invoice->invoice_date }}"
                                                 class="block mt-1 mr-4 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                                                 style="width: 80%" />
 
@@ -87,7 +88,7 @@
                                         </label>
                                     </div>
 
-                                
+
                                     {{-- Products --}}
                                     <input type="hidden" id="product-input" name="products"
                                         value="{{ json_encode($products) }}" />
@@ -99,7 +100,7 @@
                                             Confirm
                                         </button>
                                     </div>
-                                
+
 
                             </form>
 
@@ -158,8 +159,8 @@
                                 <div class="p-9">
                                     <div class="space-y-6 text-slate-700">
                                         <a class="text-lg  " href="#" id='title'>
-                                            StockFlow <img src="{{ asset('assets/img/logo_fil_rouge.svg') }}"
-                                                alt="Logo" class="h-8 w-8">
+                                            StockFlow <img src="{{ asset('assets/img/logo_fil_rouge.svg') }}" alt="Logo"
+                                                class="h-8 w-8">
                                         </a>
                                     </div>
                                 </div>
@@ -280,106 +281,6 @@
     <script>
         const productPrices = {!! json_encode($productPrices) !!};
         const productNames = {!! json_encode($productNames) !!};
-        let total = [];
-        let products = [];
-
-        const productInput = document.getElementById('product-input');
-
-        document.addEventListener('DOMContentLoaded', function() {
-            const addProductBtn = document.querySelector('.add-product-btn');
-
-            addProductBtn.addEventListener('click', function(event) {
-                event.preventDefault();
-
-                const productID = document.getElementById('select-product').value;
-                const productName = productNames[productID];
-                const quantity = document.getElementsByName('quantity')[0].value;
-                const price = productPrices[productID];
-                const total_price = (quantity * price).toFixed(2);
-                const productInput = document.getElementById('product-input');
-
-                total.push(total_price);
-                products.push({
-                    product_id: productID,
-                    quantity: quantity,
-                    total: total_price
-                });
-
-
-                productInput.value = JSON.stringify(products);
-
-
-                const newRowHtml = `
-                <tr class="border-b border-slate-200">
-                <td class="py-4 pl-4 pr-3 text-sm sm:pl-6 md:pl-0">
-                    <div class="font-medium dark:text-gray-400">${productName}</div>
-                </td>
-                <td class="px-3 py-4 text-sm text-right text-slate-500 sm:table-cell">${quantity}</td>
-                <td class="px-3 py-4 text-sm text-right text-slate-500 sm:table-cell">${price}</td>
-                <td class="px-3 py-4 text-sm text-right text-slate-500 sm:table-cell"></td>
-                <td class="py-4 pl-3 pr-4 text-sm text-right text-gray-400 sm:pr-6 md:pr-0">${total_price}</td>
-                <td class="py-4 pl-3 pr-4 text-sm text-right text-gray-400 sm:pr-6 md:pr-0">
-                    <button class="remove-product-btn text-red-500" data-index="${total.length - 1}">Remove</button>
-                </td>
-                </tr>
-                `;
-
-                const productTable = document.querySelector('.product-table');
-                productTable.insertAdjacentHTML('beforeend', newRowHtml);
-
-                document.getElementById('select-product').value = '';
-                document.getElementsByName('quantity')[0].value = '';
-
-                updateTotal();
-            });
-
-            // Ajouter un écouteur d'événements de suppression pour le bouton supprimer
-            const productTable = document.querySelector('.product-table');
-            productTable.addEventListener('click', function(event) {
-                if (event.target.classList.contains('remove-product-btn')) {
-                    const button = event.target;
-                    const index = parseInt(button.getAttribute('data-index'));
-                    const price = parseFloat(button.parentNode.previousElementSibling.textContent);
-                    total.splice(index, 1);
-
-                    const row = button.closest('tr');
-                    row.parentNode.removeChild(row);
-
-                    updateTotal();
-                }
-            });
-
-
-        });
-
-        function updateTotal() {
-            const totalCellFooter1 = document.querySelector('.total-cell1');
-            const totalCellFooter2 = document.querySelector('.total-cell2');
-            const totalInput = document.querySelector('input[name="total"]');
-            const totalValue = total.reduce((acc, curr) => parseFloat(acc) + parseFloat(curr), 0).toFixed(2);
-            totalInput.value = totalValue;
-            totalCellFooter1.textContent = totalValue + ' DH';
-            totalCellFooter2.textContent = totalValue + ' DH';
-        }
-
-        // Ajouter un écouteur d'événements de suppression pour le bouton supprimer
-        const productTable = document.querySelector('.product-table');
-        productTable.addEventListener('click', function(event) {
-            if (event.target.classList.contains('remove-product-btn')) {
-                const button = event.target;
-                const index = parseInt(button.getAttribute('data-index'));
-                const price = parseFloat(button.parentNode.previousElementSibling.textContent);
-                total.splice(index, 1);
-                products.splice(index, 1); // Supprimer le produit de la liste des produits
-
-                const row = button.closest('tr');
-                row.parentNode.removeChild(row);
-
-                updateTotal();
-
-                // Mettre à jour la valeur de l'input product-input après avoir supprimé un produit
-                productInput.value = JSON.stringify(products);
-            }
-        });
     </script>
+    <script src="{{ asset('js/invoice.js') }}"></script>
 @endsection
