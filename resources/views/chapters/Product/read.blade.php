c9f1f50dd979fef9c037c5c3b31a305d5a05f42e
Mohammed <mohammedmiessal@gmail.com>
2024-04-18 03:01:11 +0100

    Add links for the download

--- resources/views/chapters/Product/read.blade.php
+++ resources/views/chapters/Product/read.blade.php
@@ -16,12 +16,42 @@
                     <h2 class="my-6 text-2xl  text-gray-700 dark:text-gray-200">
                         Product List
                     </h2>
-                    <div class="flex justify-end mb-6">
+                    <div class="flex justify-end mb-4 ">
                         <a href="{{ route('product.create') }}">
                             <button
                                 class="px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple">
                                 Add Product
                             </button>
+                        </a>
+
+                    </div>
+                    <div class="flex justify-end mb-4 pr-2">
+                        <a href="{{ route('export') }}">
+                            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" class="w-9 h-9" viewBox="0 0 48 48">
+                                <path fill="#169154" d="M29,6H15.744C14.781,6,14,6.781,14,7.744v7.259h15V6z"></path>
+                                <path fill="#18482a" d="M14,33.054v7.202C14,41.219,14.781,42,15.743,42H29v-8.946H14z">
+                                </path>
+                                <path fill="#0c8045" d="M14 15.003H29V24.005000000000003H14z"></path>
+                                <path fill="#17472a" d="M14 24.005H29V33.055H14z"></path>
+                                <g>
+                                    <path fill="#29c27f" d="M42.256,6H29v9.003h15V7.744C44,6.781,43.219,6,42.256,6z"></path>
+                                    <path fill="#27663f" d="M29,33.054V42h13.257C43.219,42,44,41.219,44,40.257v-7.202H29z">
+                                    </path>
+                                    <path fill="#19ac65" d="M29 15.003H44V24.005000000000003H29z"></path>
+                                    <path fill="#129652" d="M29 24.005H44V33.055H29z"></path>
+                                </g>
+                                <path fill="#0c7238"
+                                    d="M22.319,34H5.681C4.753,34,4,33.247,4,32.319V15.681C4,14.753,4.753,14,5.681,14h16.638 C23.247,14,24,14.753,24,15.681v16.638C24,33.247,23.247,34,22.319,34z">
+                                </path>
+                                <path fill="#fff"
+                                    d="M9.807 19L12.193 19 14.129 22.754 16.175 19 18.404 19 15.333 24 18.474 29 16.123 29 14.013 25.07 11.912 29 9.526 29 12.719 23.982z">
+                                </path>
+                            </svg>
+                        </a>
+                        <a href="{{ route('export.csv') }}">
+                            <img width="32" height="32"
+                                src="https://img.icons8.com/external-sbts2018-outline-color-sbts2018/100/external-csv-basic-ui-elements-2.3-sbts2018-outline-color-sbts2018.png"
+                                alt="external-csv-basic-ui-elements-2.3-sbts2018-outline-color-sbts2018" />
                         </a>
                     </div>
                     <div class="w-full overflow-hidden rounded-lg shadow-xs">
@@ -89,8 +119,8 @@
                                                     {{-- Show --}}
                                                     <a href="{{ route('product.show', $product->id) }}">
                                                         <button
-                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                        aria-label="Show">
+                                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                            aria-label="Show">
                                                             <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg"
                                                                 viewBox="0 0 24 24" fill="currentColor">
                                                                 <path
@@ -119,8 +149,8 @@
                                                         @method('DELETE')
 
                                                         <button
-                                                        class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                                                        aria-label="Delete">
+                                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                                                            aria-label="Delete">
                                                             <svg class="w-4 h-4" aria-hidden="true" fill="currentColor"
                                                                 viewBox="0 0 20 20">
                                                                 <path fill-rule="evenodd"
@@ -130,7 +160,7 @@
                                                         </button>
 
                                                     </form>
-                                                {{-- </div> --}}
+                                                    {{-- </div> --}}
                                             </td>
                                         </tr>
                                     @endforeach
